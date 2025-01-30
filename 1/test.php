f5a0aedadfd9a006510f9f1f728cb66e3199eb1a
MBIOKOU <mourbiokou8@gmail.com>
2025-01-31 00:41:16 +0100

    correction du test.php

--- 1/test.php
+++ 1/test.php
@@ -9,7 +9,7 @@ libxml_disable_entity_loader (true);  // Désactiver le chargement
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_NOENT);
+$document->loadXML($xml, LIBXML_NOENT | LIBXML_NONET);
 $parsedDocument = simplexml_import_dom($document);
 
 echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8');
