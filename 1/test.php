c2cc36aa08b1423dbeea54bf64e35484e7185000
MBIOKOU <mourbiokou8@gmail.com>
2025-01-31 01:34:37 +0100

    nouvelle modif

--- 1/test.php
+++ 1/test.php
@@ -4,16 +4,12 @@ require_once('../_helpers/strip.php');
 
 // https://depthsecurity.com/blog/exploitation-xml-external-entity-xxe-injection
 
-libxml_disable_entity_loader (true);  // Désactiver le chargement 
-
-libxml_use_internal_errors(true);
-
+libxml_disable_entity_loader(true); // Désactiver le chargement des entités externes
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD);
-//$document->xinclude_libxml_flags = LIBXML_NOENT | LIBXML_NONET;
+$document->loadXML($xml, LIBXML_NOENT); // N'utilisez jamais LIBXML_DTDLOAD
 $parsedDocument = simplexml_import_dom($document);
 
-echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8');
+echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8'); // Sanitize output
 ?>
