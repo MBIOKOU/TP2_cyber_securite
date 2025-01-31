0426ce7d16bc3b22c8a3a4b870e83267b7392439
MBIOKOU <mourbiokou8@gmail.com>
2025-01-31 14:13:10 +0100

    Nouvelle modif

--- 1/test.php
+++ 1/test.php
@@ -10,7 +10,16 @@ libxml_disable_entity_loader(true);
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
+//1
+$document->xmlStandalone = true;
+
 $document->loadXML($xml, LIBXML_NOENT | LIBXML_NOCDATA | LIBXML_NONET);
+// Désactiver les DTD de manière explicite
+if ($document->doctype) {
+    $document->doctype->removeChild($document->doctype);
+}
+
+
 $parsedDocument = simplexml_import_dom($document);
 
 echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8');
