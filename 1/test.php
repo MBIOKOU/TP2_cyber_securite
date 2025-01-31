925d8122277fcd99716e6712a9d8d96533bf820b
MBIOKOU <mourbiokou8@gmail.com>
2025-01-31 01:23:10 +0100

    nouvelle modif

--- 1/test.php
+++ 1/test.php
@@ -6,11 +6,13 @@ require_once('../_helpers/strip.php');
 
 libxml_disable_entity_loader (true);  // Désactiver le chargement 
 
+libxml_use_internal_errors(true);
+
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_NOENT | LIBXML_NONET);
-$document->xinclude_libxml_flags = LIBXML_NOENT | LIBXML_NONET;
+$document->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD);
+//$document->xinclude_libxml_flags = LIBXML_NOENT | LIBXML_NONET;
 $parsedDocument = simplexml_import_dom($document);
 
 echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8');
