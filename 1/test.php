4aa6179f8e1d4d6fab44be4e30936352bec2f428
MBIOKOU <mourbiokou8@gmail.com>
2025-01-31 13:24:32 +0100

    Nouvelle modif

--- 1/test.php
+++ 1/test.php
@@ -10,7 +10,7 @@ libxml_disable_entity_loader(true);
 $xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD | LIBXML_NOCDATA);
+$document->loadXML($xml, LIBXML_NOENT | LIBXML_NONET | LIBXML_NOCDATA);
 $parsedDocument = simplexml_import_dom($document);
 
 echo htmlspecialchars($parsedDocument->content, ENT_QUOTES, 'UTF-8');
